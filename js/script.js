ef0e6df7d5a423710663c2aa8645802f606a2c8b
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-09-09 13:06:56 +0200

    Refactoring completo

--- js/script.js
+++ js/script.js
@@ -4,36 +4,36 @@
 // Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando
 //  invia il testo viene aggiunto al thread sopra, come messaggio verde
 
-
 $(document).ready(function() {
-
   // creo una funzione che al click del bottone send registra
   // il valore su una variabile
   // azzera la chat input
   $( "#send_button" ).click(function() {
-      var myText = $("#text_input").val();
-      // controllo se text contiene testo
-      if (myText != "") {
-        // $("#text_input").val("");
-        // fai una copia del template template_input_text
-        var cloudText = $(".template.input_text").clone();
-
-        // inserisci il testo text nel cloudText
-        cloudText.children($(".cloud_text p")).text(myText);
-        // rimuovo il display none
-        // cloudText.removeClass("d_none");
-        // inserisci la classe green
-        // cloudText.addClass("green");
-        // appendi il nuovo oggetto al li nella text area nell'html
-        $("ul.text_screen").append(cloudText);
-      }
-
+      sendMessage()
   });
 
-
-
-
-
+  $("#text_input").keydown(function(event){
+    if (event.which == 13) {
+      sendMessage();
+    }
+  });
 
 // end document ready
 });
+
+// my functions
+function sendMessage() {
+  var textMessage = $("#text_input").val();
+  // controllo se text contiene testo
+  if (textMessage != "") {
+    $("#text_input").val("");
+    // fai una copia del template template_input_text
+    var template = $(".template .message_row").clone();
+    // inserisci il testo text nel cloudText
+    template.find("p").text(textMessage);
+    // inserisci la classe sent
+    template.addClass("sent");
+    // appendi il nuovo oggetto al li nella text area nell'html
+    $("ul.chat_screen").append(template);
+  }
+}
