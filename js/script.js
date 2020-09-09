62e66b4b3c2e39b26308cfec8f45e0e1a59d28aa
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-09-09 15:35:05 +0200

    aggiunto millestone 2

--- js/script.js
+++ js/script.js
@@ -5,23 +5,39 @@
 //  invia il testo viene aggiunto al thread sopra, come messaggio verde
 
 $(document).ready(function() {
-  // creo una funzione che al click del bottone send registra
-  // il valore su una variabile
-  // azzera la chat input
+
   $( "#send_button" ).click(function() {
-      sendMessage()
+    sendMessage();
+    setTimeout(getReply("ok!"), 3000);
+
   });
 
   $("#text_input").keydown(function(event){
     if (event.which == 13) {
       sendMessage();
+      setTimeout(getReply("ok!"), 3000);
     }
   });
+
 
 // end document ready
 });
 
 // my functions
+
+// creo una funzione per inviare una risposta all'utente in cui dice "ok!"
+function getReply(text) {
+  // fai una copia del template template_input_text
+  var template = $(".template .message_row").clone();
+  // inserisci il testo p del template
+  template.find("p").text(text);
+  // appendi il nuovo oggetto al li nella text area nell'html
+  $("ul.chat_screen").append(template);
+}
+
+// creo una funzione cheregistra il valore su una variabile
+// azzera la chat input
+// e inserisce il valore inserito nello chat_screen dell'html
 function sendMessage() {
   var textMessage = $("#text_input").val();
   // controllo se text contiene testo
@@ -29,7 +45,7 @@ function sendMessage() {
     $("#text_input").val("");
     // fai una copia del template template_input_text
     var template = $(".template .message_row").clone();
-    // inserisci il testo text nel cloudText
+    // inserisci il testo p del template
     template.find("p").text(textMessage);
     // inserisci la classe sent
     template.addClass("sent");
---
+++ js/test.js
@@ -0,0 +1,38 @@
+//   Milestone 2
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio,
+//  l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra,
+//  vengono visualizzati solo i contatti il cui nome contiene le lettere inserite
+//  (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+
+$(document).ready(function() {
+
+  // nel momento in cui si digita qualcosa nella casella riscerca parte una funzione
+  $("#input_search").keypress(function(event){
+    // salva il valore dell'input in una variabile
+    var keyWord = $("#input_search").val();
+
+    // per ogni valore nei contact name controllo se combacia 
+    // con la keyword inserita se no applico display none
+    $('.contact_name').each(function () {
+
+      var contact = $(this).text();
+      console.log("questo contatto e' " + contact + " nell'array?");
+      var isInArray = contact.includes(keyWord);
+      console.log(isInArray);
+      if (isInArray == false) {
+        var j = $(this).parents("li").addClass("d_none");
+        console.log(j);
+      }
+
+
+  });
+
+
+  });
+
+
+// end document ready
+});
